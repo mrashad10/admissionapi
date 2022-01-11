48cdc3cef5c915f485a2a6fdec63d4a63e4ece22
Mohamad Rashad <mrashad@mrashad.com>
2022-01-11 15:35:12 +0300

    StandardJS

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express")
-const routes = require("./routes/routes")
-const rabbit = require("./models/rabbit");
+const express = require('express')
+const routes = require('./routes/routes')
+const rabbit = require('./models/rabbit')
 
 const app = express()
-app.use(express.json());
-app.use("/", routes);
+app.use(express.json())
+app.use('/', routes)
 
 app.listen(3000, () => {
-	console.log("Server is up")
-	rabbit.connect()
+  console.log('Server is up')
+  rabbit.connect()
 })
